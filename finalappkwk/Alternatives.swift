513d567b38ddaa00842e44bf0a6685f375a6ada2
alizamehdi <alizamehdi2003@gmail.com>
2022-08-05 10:47:22 -0500

    added actions and outlets

--- finalappkwk/Alternatives.swift
+++ finalappkwk/Alternatives.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class Alternatives: UITableViewController {
+class Alternatives: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,24 +23,23 @@ class Alternatives: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+
+    @IBOutlet weak var Company4Outlet: UILabel!
+    @IBOutlet weak var Company3Outlet: UILabel!
+    @IBOutlet weak var Company2Outlet: UILabel!
     @IBOutlet weak var Company1Outlet: UILabel!
     
-    @IBOutlet weak var Company2Outlet: UILabel!
-    
-    @IBOutlet weak var Company3Outlet: UILabel!
-    
-    @IBOutlet weak var Company4Outlet: UILabel!
-    
 
 
+        
     @IBAction func Company1(_ sender: Any) {
         Company1Outlet.isHidden = false
         Company2Outlet.isHidden = true
         Company3Outlet.isHidden = true
         Company4Outlet.isHidden = true
-        
     }
     
+   
     @IBAction func Company2(_ sender: Any) {
         Company2Outlet.isHidden = false
         Company1Outlet.isHidden = true
@@ -49,19 +48,21 @@ class Alternatives: UITableViewController {
         
     }
     
+    
+        
     @IBAction func Company3(_ sender: Any) {
         Company3Outlet.isHidden = false
         Company1Outlet.isHidden = true
         Company2Outlet.isHidden = true
         Company4Outlet.isHidden = true
-        
     }
     
     @IBAction func Company4(_ sender: Any) {
+       
         Company4Outlet.isHidden = false
         Company3Outlet.isHidden = true
         Company1Outlet.isHidden = true
         Company2Outlet.isHidden = true
+        
     }
-    
 }
